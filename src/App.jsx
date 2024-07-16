3789b4da3868853b43582b6745f5139c36e6b874
ClaireV38 <claireverdier00@gmail.com>
2024-07-16 17:40:29 +0200

    create pokemonCard Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import MyTitle from "./components/MyTitle"
+import PokemonCard from "./components/pokemonCard.jsx";
 
 function App() {
     return (
         <div>
-            <MyTitle />
+            <PokemonCard />
         </div>
     );
 }
