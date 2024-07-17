5411e42ecdc57f109b0582070e704cd03157df2a
ClaireV38 <claireverdier00@gmail.com>
2024-07-17 17:43:48 +0200

    quest Basics 3 display data in component

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,8 +1,25 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+
+    function displayImageOrParagraph() {
+        return pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>
+    }
+
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Elephant at sunset" />
-        <figcaption>Balbusaur</figcaption>
-    </figure>;
+        {displayImageOrParagraph()}
+        <figcaption>{pokemon.name}</figcaption>
+    </figure>
 }
 
 export default PokemonCard;
