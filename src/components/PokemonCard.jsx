0d625b7bfe7378bd9d5303589b4dae82657a6c71
ClaireV38 <claireverdier00@gmail.com>
2024-07-19 11:16:36 +0200

    update alt image

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,6 @@
 function PokemonCard() {
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Elephant at sunset" />
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image of balbusaur" />
         <figcaption>Balbusaur</figcaption>
     </figure>;
 }
